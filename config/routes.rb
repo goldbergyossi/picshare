8fb94ebbe15ba880f5c12fac439856905d1c6017
Yossi <yygold@gmail.com>
2020-04-19 09:54:57 -0400

    some minir fixes with the images

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,4 @@
 Rails.application.routes.draw do
-  
-  delete 'images/:id' => 'images#destroy', as: "delete_image"
   root 'images#index'
-  get 'images' => 'images#index'
-
-  post 'images' => 'images#create' 
-  get 'images/new' => 'images#new' 
-
-  get 'images/:id/edit' => "images#edit", as: "edit_image"
-  patch 'images/:id' => "images#update"
-
-  get 'images/:id' => 'images#show', as: 'image'
+  resources :images 
 end
